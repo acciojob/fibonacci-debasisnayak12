a056a7610280e91655c8683e8584ca721aef713f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-15 12:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	  let a = 0;
+	  let b = 1;
+	  let next;
+	  let i = 1;
+	if(num === 0){
+		return a;
+	}
+	while(i <= num){
+		next = a + b;
+		a = b;
+		b = next;
+		i++;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
