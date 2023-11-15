11e7d9e28ddd3fb596856f0d5670001cb797c967
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-15 18:40:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function fibonacci(num) {
 // your code here
+	if(num === 0 || num === 1){
+		return num;
+	}
 	  let a = 0;
 	  let b = 1;
 	  let next;
-	  let i = 2;
-	if(num === 0 || num === 1){
-		return a;
-	}
+	let i = 2;
 	while(i <= num){
 		next = a + b;
 		a = b;
